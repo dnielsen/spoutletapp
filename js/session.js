4587ed4e890209ed49ca98d7e38d1df61a5986ef
Osman Dandia <odandia@gmail.com>
2014-03-17 17:51:22 -0700

    get session page working

--- js/session.js
+++ js/session.js
@@ -1,43 +1,27 @@
 $(document).on("pagebeforeshow", "#session", function(e, data) {
 
-    var sessionId   = store.p1;
-    var eventId     = store.p2;
+    var session_id = store.p1;
     
-    var session = null;
-    var event   = null;
-    
-    for (i = 0; i < events.length; i++) {
-        if (events[i].id == eventId){
-            event = events[i];
-            break;
-        }
-    }
-    
-    if (event == null) {
-        alert('event not found!');
-    }
-    
-    for (i = 0; i < event.sessions.length; i++){
-        if (event.sessions[i].id == sessionId){
-            session = event.sessions[i];
-            break;
-        }
-    }
-    
-    if (session == null) {
-        alert('session not found!');
-    }
-    
-    if (event.avatarPath != null) {
-        $('.session-logo').attr('src', event.avatarPath);
-    } else {
-        $('.session-logo').attr('src', "images/logo-campsite-GA.png");
+    if (session_id === null) {
+        $.mobile.navigate('#home');
     }
 
-    $('.session-title').html(session.name);
-    $('.session-time').html(session.date+'<br/>'+session.time);
-    $('.session-description').html('<p>'+session.description+'</p>');
-    
-    upButton.html('<a href="#event" class="ui-btn ui-icon-arrow-l ui-btn-icon-left" data-p1='+event.id+' data-p2='+event.group+' data-direction="reverse">Event</a>');
-    
+    $.getJSON(getApiCall('sessions/'+session_id, 'fields=name,starts_at,ends_at,content,event_id'), null, function(session) {
+        
+        // if (event.avatarPath != null) {
+        //     $('.session-logo').attr('src', event.avatarPath);
+        // } else {
+        $('.session-logo').attr('src', "images/logo-campsite-GA.png");
+        // }
+
+        $('.session-title').html(session.name);
+        $('.session-time').html(getDateRangeString(session.starts_at, session.ends_at)+'<br/>'+getTimeRangeString(session.starts_at, session.ends_at));
+
+        if (session.content !== null){
+            $('.session-description').html('<p>'+session.content+'</p>');
+        }
+        
+        upButton.html('<a href="#event" class="ui-btn ui-icon-arrow-l ui-btn-icon-left" data-p1='+session.event_id+' data-direction="reverse">Event</a>');
+    });
+
 });
