d34b58b89376ca9295c3bfb9f7cf07d702bc7a26
Osman Dandia <odandia@gmail.com>
2014-07-18 10:10:12 -0700

    Show current events as upcoming

--- js/group.js
+++ js/group.js
@@ -38,7 +38,7 @@ $(document).on("pagebeforeshow", "#group", function(e, data) {
             }
         });
 
-        $.getJSON(getApiCall('events', 'fields=id,name&group_id='+group_id+'&private=0&starts_at=>'+(new Date()).toString()), function(upcomingEvents) {
+        $.getJSON(getApiCall('events', 'fields=id,name&group_id='+group_id+'&private=0&ends_at=>'+(new Date()).toString()), function(upcomingEvents) {
 
             if (upcomingEvents.length > 0) {
                 var upcomingEventsHtml = '';
@@ -53,7 +53,7 @@ $(document).on("pagebeforeshow", "#group", function(e, data) {
             }
         });
 
-        $.getJSON(getApiCall('events', 'fields=id,name&group_id='+group_id+'&private=0&ends_at=<'+(new Date()).toString()), function(pastEvents) {
+        $.getJSON(getApiCall('events', 'fields=id,name&group_id='+group_id+'&private=0&ends_at<'+(new Date()).toString()), function(pastEvents) {
 
             if (pastEvents.length > 0) {
                 var pastEventsHtml = '';
